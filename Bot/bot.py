f78fd3f7e31231f94f7310d80af6dacd48cc5409
kkostenkov <kkostenkov@gmail.com>
2017-01-04 02:56:55 +0300

    add (bot.py) bot as thread

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,5 +1,6 @@
 import os
 import config
+import threading
 import telepot
 
 from Messages import messages, palette
@@ -14,20 +15,26 @@ def message_callback(new_messages):
         callback = palette.callbacks.get(message.text, palette.default_answer)
         callback(message)
     
-    
-# Create bot instance with token
-TelegramBot = telepot.Bot(config.BOT_TOKEN)
-palette.set_bot(TelegramBot)
-# Run bot loop
-# relax param is seconds between each call to telegram serv
-TelegramBot.message_loop(message_callback, 
-                         relax=config.server_query_frequency, 
-                         timeout=120, 
-                         ordered=True, 
-                         maxhold=3, 
-                         run_forever=True
-                         )
+def launch_bot_thread():
+    # Create bot instance with token
+    TelegramBot = telepot.Bot(config.BOT_TOKEN)
+    palette.set_bot(TelegramBot)
+    # Run bot loop
+    # relax param is seconds between each call to telegram serv
+    TelegramBot.message_loop(message_callback, 
+                             relax=config.server_query_frequency, 
+                             timeout=120, 
+                             ordered=True, 
+                             maxhold=3, 
+                             run_forever=True
+                             )
+       
 
 
-# test ground
-#message_callback(messages.dict_message_example)
+worker = threading.Thread(target=launch_bot_thread,
+                                  #args=()
+                                  )
+#worker.setDaemon(True)
+worker.start()
+
+
