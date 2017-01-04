0be74e93a439d9da94f6840bcb6227ddd7596882
kkostenkov <kkostenkov@gmail.com>
2017-01-04 12:06:06 +0300

    add (lanscan) as separate thread with timer

--- Bot/lanscan/__init__.py
+++ Bot/lanscan/__init__.py
@@ -33,21 +33,44 @@ def diff():
     # Update data.
     known_online_hosts[0] = new_known_hosts
     
-    if (len(new_hosts) > 0):
-        print("________NEW ___________")
-        print (new_hosts)
-    if (len(disappeared_hosts) > 0):
-        print("________ DISAPPEARED ___________")
-        print (disappeared_hosts)
+    #if (len(new_hosts) > 0):
+    #    print("________NEW ___________")
+    #    print (new_hosts)
+    #if (len(disappeared_hosts) > 0):
+    #    print("________ DISAPPEARED ___________")
+    #    print (disappeared_hosts)
     return {
             "new" : new_hosts,
             "disappeared" : disappeared_hosts,
             }
-        
+
+scan_frequency = 10 # once every %scan_frequency% seconds 
+def lanscan_loop():
+    # do job
+    print("getting diff")
+    diff_info = diff()
+    print("got diff.")
+    new_users = diff_info.get("new", None)
+    if (new_users):
+        print("new:")
+        for user in new_users:
+            print(user + " apeared")
+    disappeared_users = diff_info.get("disappeared", None)
+    if (disappeared_users):
+        print("old:")
+        for user in disappeared:
+            print(user + " disappeared")
+    # scedule itself
+    t = threading.Timer(scan_frequency, lanscan_loop)
+    t.daemon = True
+    t.start()
+
+def run_lanscan_loop():
+    lanscan_loop()
 
 
 if __name__ == "__main__":
+    run_lanscan_loop()
     import time
-    while True:
-        diff()
+    while(True):
         time.sleep(10)
